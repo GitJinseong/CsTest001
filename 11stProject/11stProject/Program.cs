3a41c00fb9842c0c84a620be3f23ab3860582ede
DESKTOP-QO8IJTE\KGA <kcgjinseong@gmail.com>
2023-06-12 17:58:14 +0900

    upload files pm 5:58

---
+++ 11stProject/11stProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// [과제]
+// tic tac toe 게임 만들기
+// 남은 빈칸중에 랜덤하게 O를 그리고
+// 1~9 or 0~8번 받아서 선택하기
+// 선택시 O / X 표시하고 
+// 먼저 3줄 빙고 하나 만들면 승리(오목 같은 느낌)
+// 상대가 빙고를 완성하지 못하게 막을 수 있다.
+// 승리 / 패배 구현하기
+namespace _11stProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
