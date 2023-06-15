e15f0d18cc3f2ff864dc0cc69d8b9d1ee12af975
DESKTOP-QO8IJTE\KGA <kcgjinseong@gmail.com>
2023-06-15 14:27:07 +0900

    upload files pm2:27

--- 17stProject/ItemInfo.cs
+++ 17stProject/ItemInfo.cs
@@ -15,6 +15,22 @@ namespace _15stProject
                                              // 위에 아무 것도 적지 않으면 get, set 둘 다 선언된 타입으로 지정된다.
                                              // ex private int {get; set;}의 get, set은 private다.
         public int itemPrice { get; private set; }  // 이 경우 get은 public, set은 private 다.
+                                                    // 아래와 달리 { get; private set; } 이라는 빌트인(예약어)를 사용하여
+                                                    // 쉽게 구현할 수 있다.
+
+        private int _itemPrice;
+        public int itemPrice2 // 위의 public int itemPrice { get; private set; }는
+        {                     // 아래와 같이 동작한다.
+            get 
+            {
+                return _itemPrice;
+            }
+
+            private set
+            {
+                _itemPrice = value;
+            }
+        }
 
         public void InitItem(string name, int count, int price)
         {
